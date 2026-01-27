c46a7f5217b20f79cd69ee8e66adcc41e458899e
ramsey <lungahoramsey2005@gmail.com>
2026-01-27 15:03:45 +0300

    up

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -14,7 +14,7 @@ const projects = [
     title: "Zkickske",
     description:
       "An online shop that sells authentic sneakers from top brands worldwide",
-    image: "/projects/zkickske.png",
+    image: "/projects/zkicks.png",
     tags: ["Reactjs", "Typescript", "Tailwindcss"," Nextjs"],
     link: "https://zkickske.vercel.app",
     github: "https://github.com/kingzeyram/zkickske",
